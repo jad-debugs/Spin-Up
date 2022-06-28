615122840279fdb57768c84f2a104e038dbe076f
Jad Isaac <jadjrisaac@gmail.com>
2022-06-27 19:02:49 -0700

    encoder ports

--- include/subsystems/_globals.h
+++ include/subsystems/_globals.h
@@ -2,3 +2,7 @@
 
 extern okapi::Controller controller;
 extern okapi::Rate rate;
+
+extern okapi::ADIEncoder leftEncoder;
+extern okapi::ADIEncoder rightEncoder;
+extern okapi::ADIEncoder backEncoder;
