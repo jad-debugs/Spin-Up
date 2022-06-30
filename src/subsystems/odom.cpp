edc1ae26bf871f54b44e997a88e6766b2e2693a2
Jad Isaac <jadjrisaac@gmail.com>
2022-06-30 12:22:58 -0700

    trying polar method instead

--- src/subsystems/odom.cpp
+++ src/subsystems/odom.cpp
@@ -2,32 +2,33 @@
 #include <cmath>
 
 const double PI = 3.141592653589793238462643383279502884L;
+#define toRadian(theta) (PI*theta)
 
-double posX = 0;
-double posY = 0;
-double prevLeft = 0;
-double prevRight = 0;
-double prevBack = 0;
-double thetaPrev = 0;
+const double WHEEL_RADIUS = 2.0;
 const double T_l = 2.5;
 const double T_r = 2.5;
 const double T_b = 2.75;
 
+
+
+double posX = 0;
+double posY = 0;
+
+double prevLeft = 0;
+double prevRight = 0;
+double prevBack = 0;
+
+double thetaPrev = 0;
+double theta = 0;
+
 void updateOdom() {
-    double deltaLeft = (leftEncoder.get() - prevLeft) * 2 * 180 / PI;
-    double deltaRight = (rightEncoder.get() - prevRight) * 2 * 180 / PI;
-    double deltaBack = (backEncoder.get() - prevBack) * 2 * 180 / PI;
+    // get rotations number of rotations to find distane each wheel travels
+    double deltaLeft = toRadian((leftEncoder.get() - prevLeft) * WHEEL_RADIUS);
+    double deltaRight = toRadian((rightEncoder.get() - prevRight) * WHEEL_RADIUS);
+    double deltaBack = toRadian((backEncoder.get() - prevBack) * WHEEL_RADIUS);
 
-    double deltaTheta = (deltaLeft - deltaRight) / (deltaLeft + deltaRight) + thetaPrev;
+    double theta = toRadian(imu.get());
 
-    if (deltaTheta == 0) {
-        posX += deltaBack;
-        posY += deltaRight;
-    }
-    else {
-        posX += 2*sin(deltaTheta)*(deltaBack/deltaTheta + T_b);
-        posY += 2*sin(deltaTheta)*(deltaRight/deltaTheta + T_r);
-    }
 
     double prevLeft = leftEncoder.get();
     double prevRight = rightEncoder.get();
