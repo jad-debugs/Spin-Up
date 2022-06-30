cf8f151ac880d9537da82b89e08234a46f79d51c
Jad Isaac <jadjrisaac@gmail.com>
2022-06-30 12:22:34 -0700

    trying a polar method instead

--- src/subsystems/_globals.cpp
+++ src/subsystems/_globals.cpp
@@ -1,4 +1,5 @@
 #include "main.h"
+#include "okapi/impl/device/rotarysensor/IMU.hpp"
 
 okapi::Controller controller;
 okapi::Rate rate;
@@ -6,3 +7,5 @@ okapi::Rate rate;
 okapi::ADIEncoder leftEncoder = ADIEncoder({1, 'A', 'B'}, false);
 okapi::ADIEncoder rightEncoder = ADIEncoder({1, 'A', 'B'}, false);
 okapi::ADIEncoder backEncoder = ADIEncoder({1, 'A', 'B'}, false);
+
+okapi::IMU imu = IMU(1, IMUAxes::x);
